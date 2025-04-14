676b4259318457bbcd8402fcc621156f2f3b899c
dimitriusphilosophus <browndimitri2738@gmail.com>
2025-04-14 15:09:25 -0400

    - Edited shadow box pixels in teammember.css

--- scripts/teammember.js
+++ scripts/teammember.js
@@ -2,47 +2,56 @@ const everyone = [
     {
         name: "Dimitri",
         avatar: "https://pm1.narvii.com/6512/1f58cc93200885080ae84a4ec582bf01704a1db5_hq.jpg",
-        language: "Java, HTML, CSS, JavaScript"
+        language: "Java, HTML, CSS, JavaScript",
+        more: "More"
     },
     {
         name: "Christine",
         avatar: "https://pm1.narvii.com/6512/1f58cc93200885080ae84a4ec582bf01704a1db5_hq.jpg",
-        language: "Java, HTML, CSS, JavaScript"
+        language: "Java, HTML, CSS, JavaScript",
+        more: "More"
     },
     {
         name: "Rehana",
         avatar: "https://pm1.narvii.com/6512/1f58cc93200885080ae84a4ec582bf01704a1db5_hq.jpg",
-        language: "Java, HTML, CSS, JavaScript"
+        language: "Java, HTML, CSS, JavaScript",
+        more: "More"
     },
     {
         name: "Sim",
         avatar: "https://pm1.narvii.com/6512/1f58cc93200885080ae84a4ec582bf01704a1db5_hq.jpg",
-        language: "Java, HTML, CSS, JavaScript"
+        language: "Java, HTML, CSS, JavaScript",
+        more: "More"
     },
     {
         name: "Tayena",
         avatar: "https://pm1.narvii.com/6512/1f58cc93200885080ae84a4ec582bf01704a1db5_hq.jpg",
-        language: "Java, HTML, CSS, JavaScript"
+        language: "Java, HTML, CSS, JavaScript",
+        more: "More"
     },
     {
         name: "Daniel",
         avatar: "https://pm1.narvii.com/6512/1f58cc93200885080ae84a4ec582bf01704a1db5_hq.jpg",
-        language: "Java, HTML, CSS, JavaScript"
+        language: "Java, HTML, CSS, JavaScript",
+        more: "More"
     },
     {
         name: "Ishmael",
         avatar: "https://pm1.narvii.com/6512/1f58cc93200885080ae84a4ec582bf01704a1db5_hq.jpg",
-        language: "Java, HTML, CSS, JavaScript"
+        language: "Java, HTML, CSS, JavaScript",
+        more: "More"
     },
     {
         name: "Jingkang",
         avatar: "https://pm1.narvii.com/6512/1f58cc93200885080ae84a4ec582bf01704a1db5_hq.jpg",
-        language: "Java, HTML, CSS, JavaScript"
+        language: "Java, HTML, CSS, JavaScript",
+        more: "More"
     },
     {
         name: "Mahmoud",
         avatar: "https://pm1.narvii.com/6512/1f58cc93200885080ae84a4ec582bf01704a1db5_hq.jpg",
-        language: "Java, HTML, CSS, JavaScript"
+        language: "Java, HTML, CSS, JavaScript",
+        more: "More"
     },
 ];
 
@@ -59,6 +68,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 <img src="${member.avatar}" class="member-avatar" alt="${member.name}">
                 <div class="member-name">${member.name}</div>
                 <div class="member-language">${member.language}</div>
+                <div class="member-more"><button class="btn btn-dark">${member.more}</button></div>
             `;
             teamContainer.appendChild(memberElement);
         });
